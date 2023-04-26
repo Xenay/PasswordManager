7ce47b8a983cd50f50fbed1306a47cb8647b5476
Luka Grubeša <lukagrubesa26@gmail.com>
2023-04-26 18:57:40 +0200

    promjenjena enkripcijska funkcija u AES-GCM, Fernet i bcrypt opcije komentirane

--- PasswordManagerApp/views.py
+++ PasswordManagerApp/views.py
@@ -10,11 +10,54 @@ from cryptography.fernet import Fernet
 from mechanize import Browser
 import favicon
 from .models import Password
+from Crypto.Cipher import AES
+#AES-GCM route
+import base64
+import binascii, os
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from os import urandom
+#bcrypt route
+import bcrypt
+import secrets
+import ast
+#salt = bcrypt.gensalt(10)
+#hashed_key = bcrypt.hashpw(settings.KEY, salt)
+#key = hashed_key
 
+#Fernet route
 br = Browser()
 br.set_handle_robots(False)
 
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+
+backend = default_backend()
+key = urandom(16)
+nonce = urandom(12)
+
 fernet = Fernet(settings.KEY)
+
+#AES-GCM route
+#----------------------------------------------------------------------------
+# encrypt AES-GCM function
+
+def encrypt(plaintext,key):
+    aesCipher = AES.new(key,AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(plaintext)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt(ciphertext,key):
+    print(len(ciphertext))
+    (ciphertext, nonce, authTag) = ciphertext
+    
+    aesCipher = AES.new(key, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return (plaintext, None, None)
+#----------------------------------------------------------------------------
+
+
+
 
 def home(request):
     if request.method == "POST":
@@ -93,23 +136,42 @@ def home(request):
             url = request.POST.get("url")
             email = request.POST.get("email")
             password = request.POST.get("password")
-            #encrypt
-            encrypted_email = fernet.encrypt(email.encode())
 
-            encrypted_password = fernet.encrypt(password.encode())
+            #encrypt fernet
+            
+            encrypted_email = encrypt(email.encode(),key)
+            encrypted_password = encrypt(password.encode(),key)
+            print(encrypted_password)
+            
+            #encrypted_email = fernet.encrypt(email.encode())
+            #encrypted_password = fernet.encrypt(password.encode())
+            #decoded_email = base64.b64decode(encrypted_email)
+            #decoded_password = base64.b64decode(encrypted_password)
+        
+            #hash bcypt
+            #encrypted_email = bcrypt.hashpw(email.encode(), hashed_key)
+            #encrypted_password = bcrypt.hashpw(password.encode(), hashed_key)
+
+            #encrypt AES-GCM
+            # encrypt AES-GCM
+            #encrypted_email = encrypt(email)
+            #encrypted_password = encrypt(password)
+
             #get title of the website
             br.open(url)
             title = br.title()
             #link of the logo favicon
             icon = favicon.get(url)[0].url
             #save data
+            
             new_password = Password.objects.create(
                 user = request.user,
                 name = title,
                 logo = icon,
-                email = encrypted_email.decode(),
-                password = encrypted_password.decode(),
+                email = encrypted_email,
+                password = encrypted_password,
             )
+            print(encrypted_email)
             msg = "Password has been saved successfully"
             
             messages.success(request,msg)
@@ -126,8 +188,34 @@ def home(request):
     if request.user.is_authenticated:
         passwords = Password.objects.all().filter(user = request.user)
         for password in passwords:
-            password.email = fernet.decrypt(password.email.encode()).decode()
-            password.password = fernet.decrypt(password.password.encode()).decode()
+            #fernet equivalent
+            #password.email = fernet.decrypt(password.email.encode()).decode()
+            #password.password = fernet.decrypt(password.password.encode()).decode()
+#-------------------------------------------------------------------------------------
+            #bcrypt
+            #decrypted_email = bcrypt.hashpw(password.email.encode(), key).decode()
+            #decrypted_password = bcrypt.hashpw(password.password.encode(), key).decode()
+            #password.email = decrypted_email
+            #password.password = decrypted_password
+            print(ast.literal_eval(password.password))
+            #AES-GCM
+              # decrypt email and password fields
+            
+            ciphertext, nonce, authTag = decrypt(ast.literal_eval(password.password), key)
+            password.password = ciphertext.decode()
+            ciphertext, nonce, authTag = decrypt(ast.literal_eval(password.email), key)
+            password.email = ciphertext.decode()
+           
+            #cipher = AES.new(key, AES.MODE_GCM)
+
+            #nonce = decoded_email[:12]
+            #email = cipher.decrypt(nonce, decoded_email[12:], None).decode()
+            #nonce = decoded_password[:12]
+            #password = cipher.decrypt(nonce, decoded_password[12:], None).decode()
+
+           
+
+
         context = {
             "passwords": passwords,
         }
--- PasswordManagerProject/settings.py
+++ PasswordManagerProject/settings.py
@@ -132,8 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "passmanacrip@gmail.com"
-EMAIL_HOST_PASSWORD = "lopqpdiujeiuyizk"
+EMAIL_HOST_USER = "passmanacrip2@gmail.com"
+EMAIL_HOST_PASSWORD = "skogvwogcqbzfvyp"
 EMAIL_USE_TLS = True
 
 KEY = b'SiZZF1cgSt1mWsUR3_cetn0ybXLbIy63Q-FZJEoHYpQ='
